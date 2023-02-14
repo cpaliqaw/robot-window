68d48d00ed6e2e0942230b487893191c929ba04a
Chris Paliqaw <chris@qalang.com>
2023-02-14 15:23:30 +0100

    feature: support for email invitations

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,6 @@ import { createBrowserRouter, RouterProvider, Outlet, BrowserRouter, Routes, Rou
 import { ThemeProvider } from "@emotion/react";
 import { Container, createTheme, CssBaseline } from "@mui/material";
 import { SupashipUserInfo, useSession } from "./supa/use-session";
-import NavBar from "./panels/NavBar";
-import Summary from "./panels/Summary";
-import Welcome from "./panels/Welcome";
-import Layout from "./panels/Layout";
 import RobotWindowRouter from "./RobotWindowRouter";
 
 const theme = createTheme();
